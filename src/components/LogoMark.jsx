cdb08f69d3f25db4a056d3f90fb2c0bfde97dd8c
Thomas Anderson <buckybyte@gmail.com>
2026-08-22 15:02:20 +0500

    Remove logo border radius and border

--- src/components/LogoMark.jsx
+++ src/components/LogoMark.jsx
@@ -3,7 +3,7 @@ export default function LogoMark({ className = 'h-[50px] w-[50px]' }) {
     <img
       src="/logo.jpg"
       alt="Humo Racing"
-      className={className}
+      className={`${className} rounded-none border-0`}
       draggable="false"
     />
   );
